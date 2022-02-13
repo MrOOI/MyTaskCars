bf383fbc79d78d67c3c1f0dd5ee8c7c3e4baf364
MrOOI <ogabekochilov25@gmail.com>
2022-02-13 11:38:13 -0800

    BugFix

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using MyTaskCars.Enums;
 using MyTaskCars.Services;
 
 namespace MyTaskCars.Controllers
@@ -12,8 +14,18 @@ namespace MyTaskCars.Controllers
         }
         public ViewResult Index()
         {
-                
             return View("Index", _carService.GetAllCars());
+        }
+
+        public IActionResult FilterByColor(Rang color)
+        {
+
+            return View("ByColor", _carService.GetByColor(color));
+        }
+
+        public IActionResult FilterByPrice(int startPrice, int endPrice)
+        {
+            return View("Index", _carService.GetByPrice(startPrice, endPrice));
         }
     }
 }
